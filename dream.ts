6eb5a53fca6370056cfbe9d23df1f59957cb7073
diatomiccoder <chudak_93@live.com>
2017-09-25 03:20:50 +0500

    create script

---
+++ dream.ts
@@ -0,0 +1,17 @@
+let it: string = "Anything";
+
+let you = {
+    can_dream: function (dream: string) {
+        if (dream !== null && dream !== undefined) {
+            console.log("You can dream about anything)))...so you can do anything");
+            return true;
+        }
+        console.log("It's so sad(((");
+        return false;
+    },
+    can_do: {}
+}
+
+if (you.can_dream(it) === true) {
+    you.can_do[it] = true;
+}
